12c424747c9b80782b962d1094d9bfd7974c0406
githubForkkp <yp_kkp@msn.com>
2013-08-02 10:01:03 +0800

    Update detail.user.js
    up

--- detail/detail.user.js
+++ detail/detail.user.js
@@ -1,12 +1,10 @@
-﻿// ==UserScript==
+// ==UserScript==
 // @name        detail-常用验证
 // @namespace   detail.setup
 // @include     http://detail1.pre.tmall.com/item.htm*
 // @include     http://detail2.pre.tmall.com/item.htm*
 // @include  	http://detail.tmall.com/item.htm*
-// @include  	http://detail.tmall.com/venus/spu_detail.htm*
 // @match     http://detail.daily.tmall.net/item.htm*
-// @match     http://detail.daily.tmall.net/venus/spu_detail.htm*	
 // @version     1	
 // ==/UserScript==
 
@@ -30,14 +28,14 @@ document.onreadystatechange = function(){
 		
 		var ks = win.KISSY, T;
 		T = win.TShop;
-		var config = T._TMD_Config;
+		var config = T.cfg();
 		
 		var header = document.getElementById('header');
 		var script = document.getElementsByTagName('script');
 		// 
 		var Dianbaobao = matchNode('//div[@id = "dianbaobao"]').snapshotLength;
 		// 编辑宝贝
-		var edit = matchNode('//p[@id = "J_EditItem"]').snapshotLength;
+		var edit = matchNode('//span[@id = "J_EditItem"]').snapshotLength;
 		// 宝贝价格
 		var price = matchNode('//strong[@id = "J_StrPrice"]/text()').snapshotLength;
 		// 配送区域
@@ -49,41 +47,49 @@ document.onreadystatechange = function(){
 		// 累计评价
 		var rate = matchNode('//a[@id = "J_MallReviewTabTrigger"]').snapshotLength;
 		// 服务承诺
-		var service = matchNode('//dl[@class = "tb-serPromise clearfix"]').snapshotLength;
-		var _service = matchNode('//dl[@class = "tb-serPromise tm-clear"]').snapshotLength;
+		var _service = matchNode('//dd[@class = "tb-serPromise tm-laysku-dd"]').snapshotLength;
 		// 爱分享与收藏
 		var ishare = matchNode('//div[@id = "tml-share-trigger"]').snapshotLength;
 		var favorite = matchNode('//a[@id = "J_Favorite"]').snapshotLength;
 
 		// 获取detail机器
 		var server_id = matchNode('//div[@id = "server-num"]').snapshotItem(0);
+		console.info(win.g_config.ver)
 		console.info(server_id.innerHTML)
 		// 店铺优惠shoppromotion
 		var shopPromotion = matchNode('//div[@id= "J_Shoppromotions"]').snapshotLength;
-		// 搭配宝优惠
-		var mealList = matchNode('//div[@id = "J_SaleCombo"]').snapshotLength;
 		
-	// 搭配宝优惠
-	if(config.isTmallComboSupport && mealList == 0){
-		winAlert("搭配宝优惠未展示！！！");
-	}
+		
+
 
 // http请求验证 
 // （同域）静态化缓存相关
-    var dd= new ks.IO({
-        url:win.location.href,
-        success: function(){
-            var Static = dd.getResponseHeader("X-Static-Cache");
-                if (Static == "HIT" || Static == null)
-    	   { 
-    		  console.info(Static)
-    	   }
-    	       else{
-    		console.info('\u672A\u8D70\u7F13\u5B58!!'+ Static)
-    	   }
-    
-  }
-});
+//     var dd= new ks.IO({
+//         url:win.location.href,
+//         success: function(){
+//             var Static = dd.getResponseHeader("X-Static-Cache");
+//                 if (Static == "HIT" || Static == null)
+//     	   { 
+//     		  console.info(Static)
+//     	   }
+//     	       else{
+//     		console.info('\u672A\u8D70\u7F13\u5B58!!'+ Static)
+//     	   }
+
+//   }
+// });
+    // 查店铺dsr接口数据
+// var Extension = new ks.IO({
+//             url: config.apiBeans,
+//             dataType: "jsonp",
+//             success: function(d) {
+//             	Beans = d;
+//             	var dsr = "SM_368_dsr-"+config.itemDO.userId
+//             	if (Beans[dsr] !== 0) {
+//             		document.title=Beans[dsr].m+" "+Beans[dsr].s+" "+Beans[dsr].c;
+//             	};
+//             }
+//         })
 // （跨域）属性分流验证,注意有50%分桶：对应bucket_id=1~9
 // var ff = new ks.IO({
 // 	url:"http://ald.taobao.com/recommend.htm",
@@ -148,7 +154,8 @@ var wanrentuan = new ks.IO({
                     
                 }
                 // 优惠对象,针对无sku的商品
-                if(tuan.defaultModel.itemPriceResultDO.priceInfo.def.promotionList){
+                var def = tuan.defaultModel.itemPriceResultDO.priceInfo.def; 
+                if(def && def.promotionList ){
                 	var promotions = matchNode('//div[@id = "J_PromoBox"]')
                 	if (!tuan.defaultModel.itemPriceResultDO.wanrentuanInfo && promotions.snapshotItem(0).innerHTML == "" ) {
                 			console.info(tuan.defaultModel.itemPriceResultDO.priceInfo.def.promotionList)
@@ -175,21 +182,31 @@ var wanrentuan = new ks.IO({
             		if (serviceDO == 0) {
             			switch(tuan.defaultModel.serviceDO.serviceType){
             				case "3c":// 3c服务 
-            				console.info("3c服务未展示！！");
+            				winAlert("3c服务未展示！！");
             				break;
             				case "house":// 家装服务
-            				console.info("家装服务未展示！！");
+            				winAlert("家装服务未展示！！");
             				break;
             			}
             		};
 
             	}
+            	// 搭配宝优惠
+				var mealList = matchNode('//div[@id = "J_SaleCombo"]').snapshotLength;
+				if(config.isTmallComboSupport && mealList == 0){
+					winAlert("搭配宝优惠未展示！！！");
+					}
+
+
+
+				
             }
     });
 
-console.info("静态化脚本执行结束")
+console.info("异步脚本执行结束")
 
 
+	
 // 首屏区块验证
 // 避免特殊情况-可能是找不到宝贝页面
 
@@ -203,28 +220,28 @@ var nofind = function(){
 var down = function(){
 		if (postAge == 0 && _sold == 0 && rate == 0 ||house == 0 && _sold == 0 && rate == 0) {
 			return true;
-		}
+		}else{return false}
 	}
 
 		if (price == 0) {
 			// 处理拍卖
-			if(T.isBid()) console.info("detail脚本执行结束（拍卖）");return;
+			if(window.TAOBAO_PAIMAI_BIDSAU !="undefined") {console.info("detail脚本执行结束（拍卖）");return;}
 			winAlert('\u4EF7\u683C\u5C55\u793A\u4E3A\u7A7A!!');
-		}else if (!down && _service == 0 ) {
+		}else if (!down() && _service == 0 ) {
 			// if(_service == 0){
 				winAlert('\u670D\u52A1\u627F\u8BFA\u5C55\u793A\u533A\u7F3A\u5931!!');
 		}
 
 
-		if (!down && dit == 0) {
+		if (!down() && edit == 0) {
 			winAlert('\u4E3E\u62A5\u7F16\u8F91\u5B9D\u8D1D\u8D85\u94FE\u63A5\u4E0D\u5B58\u5728!!');
 		}
 
-		if (!down && _sold == 0 ) {
+		if (!down() && _sold == 0 ) {
 			winAlert('\u6708\u9500\u91CF\u5C55\u793A\u533A\u7F3A\u5931!!');
 		}
 
-		if (!down && rate == 0) {
+		if (!down() && rate == 0) {
 			winAlert('\u7D2F\u8BA1\u8BC4\u4EF7\u951A\u70B9\u7F3A\u5931!!');
 		}
 		
@@ -241,7 +258,7 @@ var down = function(){
 			}
 			console.info("detail脚本执行结束（家装）");
 			return;
-		}else if(!down && postAge == 0){
+		}else if(!down() && postAge == 0){
 			winAlert('\u914D\u9001\u533A\u57DF\u63A7\u4EF6\u7F3A\u5931!!')
 		}
 		// 车型导购项目
@@ -251,27 +268,14 @@ var down = function(){
 			winAlert("车型宝贝无多级菜单")
 		}
 	}
+		
 console.info("detail脚本执行结束")
 
 
 	}
 }
 
-// window.addEventListener('load', function() {
-// var header = document.getElementById('header');
-// var script = document.getElementsByTagName('script');
-// var APIscript =
-// matchNode('//div[@id="detail"]/script').snapshotItem(0).innerHTML;
-// console.info(APIscript);
-// APIscript.trim
-//	
-// // console.info(APIscript.snapshotItem[0].innerHTML);
-// if (script.length) {
-// winAlert("true");
-// } else {
-// winAlert("false");
-// }
-// }, true);
+
 
 // function testLinks() {
 	
@@ -292,10 +296,12 @@ console.info("detail脚本执行结束")
 // testLinks();
 // 重写alert方法
 function winAlert(msg){
-	if (typeof jAlert != "undefined")
+	if (typeof win.jAlert != "undefined")
 	{
 		win.jAlert(msg,"警告！");
 	}else{
 		alert(msg);
 	}
 }
+
+
